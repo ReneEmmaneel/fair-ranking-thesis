d58752d8a96c6f03c000d96cde802dfa70bd0ddc
Rene Emmaneel <rene.emmaneel@student.uva.nl>
2020-11-18 01:27:47 +0800

    Added tf-idf features, added BM25 features

--- framework/document_frequency.py
+++ framework/document_frequency.py
@@ -6,6 +6,7 @@
 #It will return the article id with highest relevance
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import argparse, os
 import numpy as np
@@ -21,37 +22,71 @@ def load_corpus(file, column, data = None):
     return column_data
 
 def vectorize(corpus):
+    #Generate tf-vector, idf-vector, idf-matrix and return the values
+    #Also returns the vecgtorizer object
     vectorizer = TfidfVectorizer()
-    corpus_vector = vectorizer.fit_transform(corpus)
+    fit = vectorizer.fit(corpus)
+    tfidf_matrix = vectorizer.transform(corpus)
 
-    return (vectorizer, corpus_vector)
+    tf_vectorizer = CountVectorizer()
+    tf_matrix = tf_vectorizer.fit_transform(corpus)
 
-def term_frequency(query, vectorizer, corpus_vector):
+    return (vectorizer, tf_matrix, fit.idf_, tfidf_matrix)
+
+def term_frequency_inverse_document_frequency(query, vectorizer, tfidf_matrix):
     # Get the TF-IDF scores for the query, use that to calculate
     # the relevance for each corpus_vector_row
-    # TODO: currently idf score for each word in the query is just added to
-    # create a feature score. This is (probably) not ideal
     transformed = vectorizer.transform([query])
     ctransformed = transformed.tocoo()
     query_cols = ctransformed.col
 
-    feature = np.empty([corpus_vector.get_shape()[0]])
+    feature = np.empty([tfidf_matrix.get_shape()[0]])
 
-    cvector = corpus_vector.tocoo()
-    for article in range(corpus_vector.get_shape()[0]):
+    cvector = tfidf_matrix.tocoo()
+    for article in range(tfidf_matrix.get_shape()[0]):
         score = 0
 
         for col in query_cols:
-            score += corpus_vector[article, col]
+            score += tfidf_matrix[article, col]
 
         feature[article] = score
     return feature
 
-def term_frequency_article(query_cols, corpus_vector, id):
+def term_frequency_feature(query_cols, tf_matrix, id):
     try:
         score = 0
         for col in query_cols:
-            score += corpus_vector[id, col]
+            score += tf_matrix[id, col]
+        return score
+    except IndexError:
+        return None
+
+def inverse_document_frequency_feature(query_cols, idf_vector):
+    try:
+        score = 0
+        for col in query_cols:
+            score += idf_vector[col]
+        return score
+    except IndexError:
+        return None
+
+def term_frequency_inverse_document_frequency_feature(query_cols, tfidf_matrix, id):
+    try:
+        score = 0
+        for col in query_cols:
+            score += tfidf_matrix[id, col]
+        return score
+    except IndexError:
+        return None
+
+def bm25_feature(query_cols, tf_matrix, idf_vector, doc_length_vector, doc_average, id, k = 1.2, b = 0.75):
+    try:
+        d = doc_length_vector[id, 0]
+        score = 0
+        for col in query_cols:
+            idf = idf_vector[col]
+            tf = tf_matrix[id, col]
+            score += idf * (tf * (k + 1.) / (tf + k * (1. - b + b * d / doc_average)))
         return score
     except IndexError:
         return None
@@ -64,7 +99,7 @@ if __name__ == '__main__':
     print("Query: {}".format(args.query))
 
     corpus = load_corpus('corpus_file', 'paperAbstract')
-    vectorizer, corpus_vector = vectorize(corpus)
-    feature = term_frequency(args.query, vectorizer, corpus_vector)
+    vectorizer, tf_matrix, idf_vecotr, tfidf_matrix = vectorize(corpus)
+    feature = term_frequency_inverse_document_frequency(args.query, vectorizer, tfidf_matrix)
 
     print('highest relevance is article with id: {}'.format(np.argmax(feature)))
--- framework/feature_extraction.py
+++ framework/feature_extraction.py
@@ -3,12 +3,13 @@
 #File contains various definitions of features, and functions to extract it
 
 from enum import Enum
-import document_frequency
+import document_frequency, os
 import argparse
 import loader
 import data_combining
 import pandas as pd
 import numpy as np
+import scipy
 
 #Different types of features, currently only inverse document frequency
 class Type(Enum):
@@ -24,8 +25,14 @@ class Feature:
     def process_query(self, query):
         pass
 
-    def get_feature(self, id):
+    def get_features(self, id):
         pass
+
+    def get_name(self):
+        return "unnamed feature"
+
+    def get_feature_names(self):
+        return [get_name()]
 
 class IDF_Feature(Feature):
     def __init__(self, file, column, data):
@@ -36,9 +43,20 @@ class IDF_Feature(Feature):
         self.file = file
         self.column = column
         corpus = document_frequency.load_corpus(self.file, self.column, data)
-        vectorizer, corpus_vector = document_frequency.vectorize(corpus)
+        vectorizer, tf_matrix, idf_vector, tfidf_matrix = document_frequency.vectorize(corpus)
         self.vectorizer = vectorizer
-        self.corpus_vector = corpus_vector
+        self.tf_matrix = tf_matrix
+        self.idf_vector = idf_vector
+        self.tfidf_matrix = tfidf_matrix
+
+        self.doc_length_vector = scipy.sparse.csr_matrix.sum(self.tf_matrix, axis=1)
+        self.doc_average = self.doc_length_vector.mean()
+
+    def get_name(self):
+        return "IDF feature"
+
+    def get_feature_names(self):
+        return ['TF(q, d) in {}'.format(self.column), 'IDF(q) in {}'.format(self.column), 'TF_IDF(q, d) in {}'.format(self.column), 'BM25 of {}'.format(self.column)]
 
     def process_query(self, query):
         self.get_vectorize(query)
@@ -50,10 +68,14 @@ class IDF_Feature(Feature):
         self.query_cols = ctransformed.col
 
     def get_vectorize(self, query):
-        return document_frequency.term_frequency(query, self.vectorizer, self.corpus_vector)
+        return document_frequency.term_frequency_inverse_document_frequency(query, self.vectorizer, self.tfidf_matrix)
 
-    def get_feature(self, id):
-        return document_frequency.term_frequency_article(self.query_cols, self.corpus_vector, id)
+    def get_features(self, id):
+        tf_feature = document_frequency.term_frequency_feature(self.query_cols, self.tf_matrix, id)
+        idf_feature = document_frequency.inverse_document_frequency_feature(self.query_cols, self.idf_vector)
+        tfidf_feature = document_frequency.term_frequency_inverse_document_frequency_feature(self.query_cols, self.tfidf_matrix, id)
+        bm25 = document_frequency.bm25_feature(self.query_cols, self.tf_matrix, self.idf_vector, self.doc_length_vector, self.doc_average, id)
+        return [tf_feature, idf_feature, tfidf_feature, bm25]
 
 class LEN_Feature(Feature):
     def __init__(self, file, column, data):
@@ -66,20 +88,52 @@ class LEN_Feature(Feature):
         corpus = document_frequency.load_corpus(self.file, self.column, data)
         self.lengths = [len(c) for c in corpus]
 
-    def get_feature(self, id):
-        return(self.lengths[id])
+    def get_features(self, id):
+        return([self.lengths[id]])
 
-def training_file_to_libsvm(input_file, data, output_file, verbose = False, max_rows = None):
+    def get_name(self):
+        return "length feature"
+
+    def get_feature_names(self):
+        return ["LEN(d) of {}".format(self.column)]
+
+def int_if_possible(str_to_int):
+    try:
+        return int(str_to_int)
+    except ValueError:
+        return None
+
+class NUM_Feature(Feature):
+    def __init__(self, file, column, data):
+        self.query_dependant = False
+        self.document_dependant = True
+
+        self.type = Type.IDF
+        self.file = file
+        self.column = column
+
+        file_data = data[file]
+        column_data = file_data.loc[:, column]
+        self.nums = [int_if_possible(d) for d in column_data]
+
+    def get_features(self, id):
+        return([self.nums[id]])
+
+    def get_name(self):
+        return "number feature"
+
+    def get_feature_names(self):
+        return ["num of {}".format(self.column)]
+
+def training_file_to_libsvm(input_file, data, output_file, features, verbose = False, max_rows = None):
     training_data = pd.read_json(input_file, lines = True)
 
     #empty output file
-    open(output_file, 'w').close()
-
-    features = [IDF_Feature('corpus_file', 'paperAbstract', data),
-                IDF_Feature('corpus_file', 'title', data),
-                LEN_Feature('corpus_file', 'paperAbstract', data),
-                LEN_Feature('corpus_file', 'title', data)
-                ]
+    try:
+        open(output_file, 'w').close()
+    except FileNotFoundError:
+        print("FileNotFoundError: Directory probably does not exist")
+        return
 
     for index, row in training_data.iterrows():
         if verbose:
@@ -100,13 +154,14 @@ def training_file_to_libsvm(input_file, data, output_file, verbose = False, max_
             feature_vector = []
 
             for index, f in enumerate(features):
-                new_feature = f.get_feature(id)
-                if new_feature == None:
-                    continue
-                feature_vector.append(new_feature)
+                new_features = f.get_features(id)
+                if new_features == None:
+                    print('error on line {}, feature {}'.format(index, f.get_name()))
+                feature_vector.extend(new_features)
+
 
             with open(output_file, "a") as out:
-                out.write("{} {}\n".format(relevance, ' '.join('{}:{}'.format(i, f) for i, f in enumerate(feature_vector))))
+                out.write("{} {}\n".format(relevance, ' '.join('' if f is None else '{}:{}'.format(i, f) for i, f in enumerate(feature_vector))))
 
     if verbose:
         print("\nFeature extraction done! Saved in libsvm format to {}".format(output_file))
@@ -120,4 +175,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data = loader.parse_files()
-    training_file_to_libsvm(args.infile, data, args.outfile, verbose = True)
+
+
+
+    features = [IDF_Feature('corpus_file', 'paperAbstract', data),
+                IDF_Feature('corpus_file', 'title', data),
+                IDF_Feature('corpus_file', 'journalName', data),
+                IDF_Feature('corpus_file', 'venue', data),
+                LEN_Feature('corpus_file', 'paperAbstract', data),
+                LEN_Feature('corpus_file', 'title', data),
+                LEN_Feature('corpus_file', 'journalName', data),
+                LEN_Feature('corpus_file', 'venue', data),
+                NUM_Feature('paper_file', 'n_citations', data)
+                ]
+
+    print('Features:')
+    feature_names = [f.get_feature_names() for f in features]
+    feature_names_flat = [item for sublist in feature_names for item in sublist]
+    print('\n'.join('{}:\t{}'.format(i, f) for i, f in enumerate(feature_names_flat)))
+
+    training_file_to_libsvm(args.infile, data, args.outfile, features, verbose = True)
