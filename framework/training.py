69e254257d8d4375cd5074adf420f01a9511e0c5
Rene Emmaneel <rene.emmaneel@student.uva.nl>
2020-11-18 22:55:46 +0800

    Continued working on rankSVM

--- framework/training.py
+++ framework/training.py
@@ -6,25 +6,22 @@
 
 import itertools
 import numpy as np
-import argparse
+import argparse, os
+import matplotlib.pyplot as plt
 from sklearn import svm, linear_model, model_selection, datasets, preprocessing
+from warnings import filterwarnings
+import pandas as pd
+import pickle
+filterwarnings('ignore')
 
-def transform_pairwise(X, y):
-    """Transforms data into pairs with balanced labels for ranking
-    Transforms a n-class ranking problem into a two-class classification
-    problem. Subclasses implementing particular strategies for choosing
-    pairs should override this method.
+def import_libsvm(file):
+    """Given a file in libsvm format, transform it into pairs of relevant/irrelevant
     In this method, all pairs are choosen, except for those that have the
-    same target value. The output is an array of balanced classes, i.e.
+    same relevance value. The output is an array of balanced classes, i.e.
     there are the same number of -1 as +1
     Parameters
     ----------
-    X : array, shape (n_samples, n_features)
-        The data
-    y : array, shape (n_samples,) or (n_samples, 2)
-        Target labels. If it's a 2D array, the second column represents
-        the grouping of samples, i.e., samples with different groups will
-        not be considered.
+    file : file in libsvm format. Libsvm format is n lines with relevance and feature vector
     Returns
     -------
     X_trans : array, shape (k, n_feaures)
@@ -32,26 +29,6 @@ def transform_pairwise(X, y):
     y_trans : array, shape (k,)
         Output class labels, where classes have values {-1, +1}
     """
-    X_new = []
-    y_new = []
-    y = np.asarray(y)
-    if y.ndim == 1:
-        y = np.c_[y, np.ones(y.shape[0])]
-    comb = itertools.combinations(range(X.shape[0]), 2)
-    for k, (i, j) in enumerate(comb):
-        if y[i, 0] == y[j, 0] or y[i, 1] != y[j, 1]:
-            # skip if same target or different group
-            continue
-        X_new.append(X[i] - X[j])
-        y_new.append(np.sign(y[i, 0] - y[j, 0]))
-        # output balanced classes
-        if y_new[-1] != (-1) ** k:
-            y_new[-1] = - y_new[-1]
-            X_new[-1] = - X_new[-1]
-
-    return np.asarray(X_new), np.asarray(y_new).ravel()
-
-def import_libsvm(file):
     data = datasets.load_svmlight_file(file)
     X = data[0].toarray()
     y = data[1]
@@ -61,7 +38,7 @@ def import_libsvm(file):
     comb = itertools.combinations(range(len(X)), 2)
     for k, (i, j) in enumerate(comb):
         if y[i] == y[j]:
-            # skip if same target or different group
+            # skip if same relevance
             continue
         X_new.append(X[i] - X[j])
         y_new.append(np.sign(y[i] - y[j]))
@@ -80,7 +57,7 @@ class RankSVM(svm.LinearSVC):
     See object :ref:`svm.LinearSVC` for a full description of parameters.
     """
 
-    def fit(self, X_trans, y_trans):
+    def fit(self, X_trans, y_trans, max_iter = 1000):
         """
         Fit a pairwise ranking model.
         Parameters
@@ -92,10 +69,46 @@ class RankSVM(svm.LinearSVC):
         -------
         self
         """
-        self.max_iter = 1000
-        self.verbose = 1
+        self.max_iter = max_iter
         super(RankSVM, self).fit(X_trans, y_trans)
         return self
+
+    def fit_and_plot(self, X_trans_train, y_trans_train, X_trans_test, y_trans_test):
+        for i in range(100):
+            self.max_iter = 20
+
+        # Create a template lit to store accuracies
+        acc = []
+
+        iterations = np.logspace(0,3, num = 6)
+
+        # Iterate along a logarithmically spaced ranged
+        for i in iterations:
+            # Print out the number of iterations to use for the current loop
+            # Create an SVC algorithm with the number of iterations for the current loop
+            self.max_iter = i
+            # Fit the algorithm to the data
+            super(RankSVM, self).fit(X_trans_train, y_trans_train)
+            # Append the current accuracy score to the template list
+            score = self.score(X_trans_test, y_trans_test)
+            print('Score after {} iterations: {}'.format(int(i),score))
+            acc.append(score)
+
+        # Convert the accuracy list to a series
+        acc = pd.Series(acc, index = iterations)
+        # Set the plot size
+        plt.figure(figsize = (15,10))
+        # Set the plot title
+        title = 'Graph to show the accuracy of the SVC model as number of iterations increases\nfinal accuracy: ' + str(acc.iloc[-1])
+        plt.title(title)
+        # Set the xlabel and ylabel
+        plt.xlabel('Number of iterations')
+        plt.ylabel('Accuracy score')
+        # Plot the graph
+        acc.plot.line()
+        plt.show()
+
+
 
     def predict(self, X):
         """
@@ -127,18 +140,31 @@ if __name__ == '__main__':
     # and print the performance of ranking vs linear regression
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", dest="libsvm_file", required=True,
+    parser.add_argument("-f", "--file", dest="libsvm_file", required=False,
                         help="training data in libsvm format", metavar="FILE")
+    parser.add_argument("-p", "--pickle", dest="pickle_folder", required=False,
+                        help="dump pickle info", metavar="FOLDER")
 
     args = parser.parse_args()
 
-    X_trans, y_trans = import_libsvm(args.libsvm_file)
-    X_trans = preprocessing.scale(X_trans)
-    print(X_trans[0:25])
+    if args.libsvm_file:
+        X_trans, y_trans = import_libsvm(args.libsvm_file)
+        #X_trans = preprocessing.scale(X_trans)
+
+        if args.pickle_folder:
+            pickle.dump(X_trans, open(args.pickle_folder + 'X_trans', 'wb'))
+            pickle.dump(y_trans, open(args.pickle_folder + 'y_trans', 'wb'))
+    elif args.pickle_folder:
+        X_trans = pickle.load(open(args.pickle_folder + 'X_trans', 'rb'))
+        y_trans = pickle.load(open(args.pickle_folder + 'y_trans', 'rb'))
+    else:
+        print('Specify either libsvm_file or picle_folder')
+        os._exit(0)
 
     print('loaded! now for training')
-    kf = model_selection.KFold(2)
-    # print the performance of ranking
-    for train_index, test_index in kf.split(X_trans):
-        rank_svm = RankSVM().fit(X_trans[train_index], y_trans[train_index])
-        print('Performance of ranking {}'.format(rank_svm.score(X_trans[test_index], y_trans[test_index])))
+    X_trans_train, X_trans_test, y_trans_train, y_trans_test = model_selection.train_test_split(X_trans, y_trans, train_size=0.1)
+
+    #Train the model, and print the performance of the model. If you want to plot the performance over iterations, use:
+    #RankSVM().fit_and_plot(X_trans_train, y_trans_train, X_trans_test, y_trans_test)
+    rank_svm = RankSVM().fit(X_trans_train, y_trans_train, max_iter = 1000)
+    print('Performance of ranking {}'.format(rank_svm.score(X_trans_test, y_trans_test)))
